45dd2aaff022c84e308bcfd8032129383da472cd
Andres Leon <116218857+aleontepixtle@users.noreply.github.com>
2022-10-25 18:15:36 -0400

    Update workday_program.py

--- workday_program.py
+++ workday_program.py
@@ -1,6 +1,11 @@
-import employeeClass as EMP
+# Author: Andres Leon T
+# Date: 03/2021
+# Purpose: This program creats employee objects and asks for user input for their attributes.
+# The program will ask if the employee is a supervisor and will increase their pay if they met the bonus goal.
+# Then it will neatlty print the details of the employee/supervisor.
+import employeeClass as EMP # imports employee class from seperate file
 
-def createEmployee():
+def createEmployee(): # create employee object
     """self, empName, empNumber, shiftNum, empSalary"""
     empName = input("Enter employee Name:\t")
     empNumber = input("Enter employee Number:\t ")
@@ -9,18 +14,18 @@ def createEmployee():
     newEmployee = EMP.Employee(empName, empNumber, shiftNum, empSalary)
     return newEmployee
 
-def createSuperVisor(newEmployee):
+def createSuperVisor(newEmployee): # creates supervisor object
     """empName, empNumber, shiftNum, empSalary, bonusPay, metGoal"""
     goalCheck = input('Did supervisor ' + newEmployee.get_Name(newEmployee) +
                     ' meet the production goal? Y/N ').upper()
-    if goalCheck == 'Y':
+    if goalCheck == 'Y': # checks for goal requirement
         metGoal = True
-        bonusPay = 7500 + newEmployee.empSalary
+        bonusPay = 7500 + newEmployee.empSalary # increases base pay
     else:
         metGoal = False
         bonusPay = 0 + newEmployee.empSalary
         
-    newSupervisor = EMP.Supervisor(newEmployee.get_Name(newEmployee),
+    newSupervisor = EMP.Supervisor(newEmployee.get_Name(newEmployee), 
         newEmployee.get_Number(newEmployee),                        
         newEmployee.get_shiftNum(newEmployee),
         newEmployee.get_empSalary(newEmployee),
@@ -29,7 +34,7 @@ def createSuperVisor(newEmployee):
     
     return newSupervisor
            
-def supervisorCheck(newEmployee):
+def supervisorCheck(newEmployee): # asks user for if employee is a supervisor
     
     userinput = input("Is " + newEmployee.get_Name(newEmployee) + " a supervisor? Y/N ").upper()
     if userinput == 'Y':
@@ -38,7 +43,7 @@ def supervisorCheck(newEmployee):
         return False
 
     
-def main():
+def main(): # main creates employee/supervisor based on user input for type of employee
     newEmployee = createEmployee()
     superFlag = supervisorCheck(newEmployee)
     
